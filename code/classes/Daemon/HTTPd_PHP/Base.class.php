453cd23465236bcec4eca9a03ae1ca706163f94a
Mark Karpeles <mark@hell.ne.jp>
2009-01-06 17:20:50 +0000

    First basic HTTP server emulation using runkit sandbox
    git-svn-id: https://ookoo.org/svn/pinetd2/trunk@215 bde33f66-d171-dc11-b289-00304882fe80

--- code/classes/Daemon/HTTPd_PHP/Base.class.php
+++ code/classes/Daemon/HTTPd_PHP/Base.class.php
@@ -8,5 +8,13 @@ class Base extends \Daemon\HTTPd\Base {
 		parent::__construct($id, $daemon, $IPC, $node);
 		$this->files_base = PINETD_CLASS_ROOT.'/Daemon/'.$this->daemon['Daemon'].'/files'; // we do not sanitarize "Daemon" as it's entered by the admin anyway
 	}
+
+	public function _ChildIPC_getFilesBase() {
+		return $this->files_base;
+	}
+
+	public function getFilesBase() {
+		return $this->files_base;
+	}
 }
 
--- code/classes/Daemon/HTTPd_PHP/Client.class.php
+++ code/classes/Daemon/HTTPd_PHP/Client.class.php
@@ -1,16 +1,69 @@
 <?php
 
 namespace Daemon\HTTPd_PHP;
+use \Runkit_Sandbox;
 
 class Client extends \Daemon\HTTPd\Client {
+	protected $headers_sent = false;
+
 	public function handleRequest($request, $headers, $cookies) {
-		// We got a request, fill some fake stuff to make it look like a legitimate one :)
+		// We got a request, prepare a sandbox :)
+		$base = $this->IPC->getFilesBase();
+		$options = array(
+			'open_basedir' => $base.':/tmp',
+			'allow_url_fopen' => false,
+			'runkit.internal_override' => false,
+		);
+		$sandbox = new Runkit_Sandbox($options);
+		$sandbox['output_handler'] = array($this, '_outputHandler');
+
+		$path = parse_url($request['path']);
+
+		// build vars for $_SERVER
+		$s_vars = array(
+			'REMOTE_ADDR' => $this->peer[0],
+			'SERVER_SIGNATURE' => $this->getVersionString(),
+			'SERVER_SOFTWARE' => 'PInetd',
+			'DOCUMENT_ROOT' => $base,
+			'REMOTE_PORT' => $this->peer[1],
+			'GATEWAY_INTERFACE' => 'CGI/1.1',
+			'SERVER_PROTOCOL' => 'HTTP/'.$request['version'],
+			'REQUEST_METHOD' => $request['method'],
+			'QUERY_STRING' => $path['query'],
+			'REQUEST_URI' => $request['path'],
+			'SCRIPT_NAME' => $path['path'],
+			'PHP_SELF' => $path['path'],
+		);
 		foreach($headers as $head => $data) {
 			$head = 'HTTP_'.preg_replace('/[^A-Z0-9_]/', '_', strtoupper($head));
-			$_SERVER[$head] = $data[0][1];
+			$s_vars[$head] = $data[0][1];
 		}
-		var_dump($_SERVER);
-		parent::handleRequest($request, $headers, $cookies);
+		$g_vars = array();
+		parse_str((string)$path['query'], $g_vars);
+		$sandbox->eval('foreach('.var_export($s_vars, true).' as $var => $val) $_SERVER[$var] = $val; unset($var, $val);');
+		$sandbox->eval('$_GET = '.var_export($g_vars, true).';');
+		$sandbox->eval('$_ENV = array();'); // avoid providing data from $_ENV
+
+		$sandbox->eval('phpinfo();');
+		//$sandbox->eval('echo \'Hello \'.$_SERVER[\'REMOTE_ADDR\'].\'!<br /><pre>\'; var_dump($_SERVER);');
+
+		$this->close();
+
+//		var_dump($request);
+	}
+
+	public function _outputHandler($str) {
+		// check if headers sent
+		if (!$this->headers_sent) {
+			$headers = 'HTTP/1.0 200 Ok'."\r\n";
+			$headers.= 'Server: '.$this->getVersionString()."\r\n";
+			$headers.= 'Content-Type: text/html'."\r\n";
+			$this->sendMsg($headers."\r\n");
+			$this->headers_sent = true;
+		}
+
+		$this->sendMsg($str);
+		return '';
 	}
 }
 
