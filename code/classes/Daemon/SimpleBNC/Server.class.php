4000fc8d246016f9b60a9e3b82b094d76802e7c2
Grepsd <grepsd@ookoo.org>
2009-01-08 17:17:59 +0000

    Rebuild Parent System
    git-svn-id: https://ookoo.org/svn/pinetd2/trunk@223 bde33f66-d171-dc11-b289-00304882fe80

--- code/classes/Daemon/SimpleBNC/Server.class.php
+++ code/classes/Daemon/SimpleBNC/Server.class.php
@@ -5,10 +5,16 @@ use pinetd\Logger;
 use pinetd\SQL;
 
 class Server extends \pinetd\TCP\Base {
-	function spawnClient($socket, $peer, $parent, $protocol) {
+    
+    public function __construct() {
+        call_user_func(array('parent', '__construct'), func_get_args());
+
         $this->sql = SQL::Factory($this->localConfig['Storage']);
         $this->IPC->createPort('SimpleBNC::Parent', $this);
+    }
 
+
+    function spawnClient($socket, $peer, $parent, $protocol) {
         $class = relativeclass($this, 'Server_Thread');
         return new $class($socket, $peer, $parent, $protocol);
 	}
