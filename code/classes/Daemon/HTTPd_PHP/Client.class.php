c34501df6352e1ae458f5d723c506f796eb15fc8
Mark Karpeles <mark@hell.ne.jp>
2009-01-12 18:07:44 +0000

    removed code
    git-svn-id: https://ookoo.org/svn/pinetd2/trunk@227 bde33f66-d171-dc11-b289-00304882fe80

--- code/classes/Daemon/HTTPd_PHP/Client.class.php
+++ code/classes/Daemon/HTTPd_PHP/Client.class.php
@@ -1,22 +1,12 @@
 <?php
 
 namespace Daemon\HTTPd_PHP;
-use \Runkit_Sandbox;
 
 class Client extends \Daemon\HTTPd\Client {
 	protected $headers_sent = false;
 
 	public function handleRequest($request, $headers, $cookies) {
 		// We got a request, prepare a sandbox :)
-		$base = $this->IPC->getFilesBase();
-		$options = array(
-			'open_basedir' => $base.':/tmp',
-			'allow_url_fopen' => false,
-			'runkit.internal_override' => false,
-		);
-
-		$sandbox = new Runkit_Sandbox($options);
-		$sandbox['output_handler'] = array($this, '_outputHandler');
 
 		$path = parse_url($request['path']);
 
@@ -39,17 +29,12 @@ class Client extends \Daemon\HTTPd\Client {
 			$head = 'HTTP_'.preg_replace('/[^A-Z0-9_]/', '_', strtoupper($head));
 			$s_vars[$head] = $data[0][1];
 		}
+
+		// GET
 		$g_vars = array();
 		parse_str((string)$path['query'], $g_vars);
-		$sandbox->eval('foreach('.var_export($s_vars, true).' as $var => $val) $_SERVER[$var] = $val; unset($var, $val);');
-		$sandbox->eval('$_GET = '.var_export($g_vars, true).';');
-		$sandbox->eval('$_ENV = array();'); // avoid providing data from $_ENV
-		$sandbox->eval('chdir('.var_export($base, true).');');
 
-		$sandbox->require($base.'/index.php');
-
-		//$sandbox->eval('phpinfo();');
-		//$sandbox->eval('echo \'Hello \'.$_SERVER[\'REMOTE_ADDR\'].\'!<br /><pre>\'; var_dump($_SERVER);');
+		// what shall I do there? TODO!
 
 		$this->close();
 
