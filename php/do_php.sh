13c766a7d18f5997a3293e1898ce7ca832abd012
Mark Karpeles <mark@hell.ne.jp>
2009-01-12 17:13:00 +0000

    removed runkit [useless]
    git-svn-id: https://ookoo.org/svn/pinetd2/trunk@226 bde33f66-d171-dc11-b289-00304882fe80

--- php/do_php.sh
+++ php/do_php.sh
@@ -19,50 +19,6 @@ cd "$PHP_VERSION"
 
 NEED_AUTOCONF=no
 
-if [ ! -d "ext/runkit" ]; then
-	echo -n "Getting runkit..."
-	# login to anoncvs
-	if [ -f ~/.cvspass ]; then
-		if [ `grep -c ':pserver:cvsread@cvs.php.net:2401/repository' ~/.cvspass 2>/dev/null` -eq 0 ]; then
-			echo "/1 :pserver:cvsread@cvs.php.net:2401/repository A" >>~/.cvspass
-		fi
-	else
-		echo "/1 :pserver:cvsread@cvs.php.net:2401/repository A" >>~/.cvspass
-	fi
-	# get runkit
-	cvs -Q -d :pserver:cvsread@cvs.php.net:2401/repository checkout pecl/runkit
-	# I hate cvs
-	mv pecl/runkit ext/runkit
-	rm -fr pecl
-	# fix ZVAL_REFCOUNT which became Z_REFCOUNT_P in 5.3.0 (maybe?)
-	sed -i 's/ZVAL_REFCOUNT/Z_REFCOUNT_P/' ext/runkit/runkit.c
-	sed -i 's/member->refcount = 1/Z_SET_REFCOUNT_P(member, 1)/' ext/runkit/runkit_sandbox_parent.c
-	sed -i 's/(pzv)->refcount = 1/Z_SET_REFCOUNT_P(pzv, 1)/' ext/runkit/php_runkit.h
-	sed -i 's/(pzv)->is_ref = 0/Z_UNSET_ISREF_P(pzv)/' ext/runkit/php_runkit.h
-	# fix for change in sapi header_handler
-	sed -i 's/php_runkit_sandbox_sapi_header_handler(sapi_header_struct \*sapi_header,sapi_headers_struct/php_runkit_sandbox_sapi_header_handler(sapi_header_struct *sapi_header,sapi_header_op_enum op,sapi_headers_struct/' ext/runkit/runkit_sandbox.c
-	sed -i 's/php_runkit_sandbox_original_sapi.header_handler(sapi_header, sapi_headers/php_runkit_sandbox_original_sapi.header_handler(sapi_header, op, sapi_headers/' ext/runkit/runkit_sandbox.c
-	for foo in ext/runkit/runkit_sandbox.c ext/runkit/runkit.c ext/runkit/runkit_sandbox_parent.c ext/runkit/runkit_constants.c ext/runkit/runkit_import.c ext/runkit/runkit_props.c; do
-		sed -i -r -e 's/([a-z_]+)->refcount = ([^;]+)/Z_SET_REFCOUNT_P(\1, \2)/' "$foo"
-		sed -i -r -e 's/([a-z_]+)\.refcount = ([^;]+)/Z_SET_REFCOUNT(\1, \2)/' "$foo"
-		sed -i -r -e 's/([a-z_]+)->refcount--/Z_SET_REFCOUNT_P(\1, Z_REFCOUNT_P(\1) - 1)/' "$foo"
-		sed -i -r -e 's/([a-z_]+)->refcount\+\+/Z_SET_REFCOUNT_P(\1, Z_REFCOUNT_P(\1) - 1)/' "$foo"
-		sed -i -r -e 's/([a-z_]+)->is_ref = 0/Z_UNSET_ISREF_P(\1)/' "$foo"
-		sed -i -r -e 's/([a-z_]+)\.is_ref = 0/Z_UNSET_ISREF(\1)/' "$foo"
-		sed -i -r -e 's/([a-z_]+)->is_ref/Z_ISREF_P(\1)/' "$foo"
-		sed -i -r -e 's/zend_is_callable\(([^)]*)\)/zend_is_callable(\1 TSRMLS_CC)/' "$foo"
-		sed -i -r -e 's/zend_file_handle_dtor\(([^)]*)\)/zend_file_handle_dtor(\1 TSRMLS_CC)/' "$foo"
-		# for stuff like if ((*symtable)->refcount > 1 && ...
-		sed -i -r -e 's/\(\*([a-z_]+)\)->refcount\+\+/Z_SET_REFCOUNT_PP(\1, Z_REFCOUNT_PP(\1) + 1)/' "$foo"
-		sed -i -r -e 's/\(\*([a-z_]+)\)->is_ref = 1/Z_SET_ISREF_PP(\1)/' "$foo"
-		sed -i -r -e 's/\(\*([a-z_]+)\)->refcount/Z_REFCOUNT_PP(\1)/' "$foo"
-		sed -i -r -e 's/\(\*([a-z_]+)\)->is_ref/Z_ISREF_PP(\1)/' "$foo"
-		sed -i 's/ZVAL_ADDREF/Z_ADDREF_P/' "$foo"
-	done
-	echo "done"
-	NEED_AUTOCONF=yes
-fi
-
 if [ ! -d "ext/ares" ]; then
 	echo -n "Getting ares..."
 	# login to anoncvs
@@ -126,7 +82,7 @@ echo -n "Configuring..."
  --with-gd --with-jpeg-dir=/usr/lib --with-png-dir --with-zlib --enable-gd-native-ttf \
  --with-mysql="$MYSQLI_DIR" --with-mysqli="$MYSQLI_PATH" --with-mhash --with-config-file-path="$BUILD_ROOT" \
  --enable-libxml --enable-dom --enable-xml --enable-xmlreader --enable-xmlwriter --with-openssl=/usr \
- --with-imap=/usr --with-imap-ssl --enable-maintainer-zts --enable-runkit --enable-runkit-sandbox --with-ares
+ --with-imap=/usr --with-imap-ssl --with-ares
 
 if [ x"$?" != x"0" ]; then
 	echo "FAILED"
