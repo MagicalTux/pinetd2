23f97c530f1948f44ce9c62f79a1f0cab5e59284
Mark Karpeles <mark@hell.ne.jp>
2009-04-11 12:12:12 +0000

    fixing ra flag
    git-svn-id: https://ookoo.org/svn/pinetd2/trunk@337 bde33f66-d171-dc11-b289-00304882fe80

--- code/classes/Daemon/DNSd/Engine.class.php
+++ code/classes/Daemon/DNSd/Engine.class.php
@@ -95,7 +95,7 @@ class Engine {
 				$pos = strpos($domain, '.');
 				if ($pos === false) {
 					$pkt->setFlag('rcode', Packet::RCODE_REFUSED); // We do not want to resolve you (won't recursive resolve)
-					$pkt->setFlag('ra', 1);
+					$pkt->setFlag('ra', 0);
 					return;
 				}
 				$host .= ($host==''?'':'.').substr($domain, 0, $pos);
