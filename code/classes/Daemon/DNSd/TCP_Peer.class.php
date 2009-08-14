f7bdc812d80075ec7200bdf47624c7a32d8a7343
Mark Karpeles <mark@hell.ne.jp>
2009-08-14 03:44:35 +0000

    Fixed bug with uniqueness of IPC
    git-svn-id: https://ookoo.org/svn/pinetd2/trunk@374 bde33f66-d171-dc11-b289-00304882fe80

--- code/classes/Daemon/DNSd/TCP_Peer.class.php
+++ code/classes/Daemon/DNSd/TCP_Peer.class.php
@@ -20,7 +20,7 @@ class TCP_Peer extends \pinetd\TCP\Client {
 	protected function receivePacket($pkt) {
 		$data = unserialize($pkt);
 		try {
-			$res = $this->IPC->callPort('DNSd::DbEngine::'.$this->unique(), $data[0], $data[1]);
+			$res = $this->IPC->callPort('DNSd::DbEngine::'.$this->unique, $data[0], $data[1]);
 		} catch(Exception $e) {
 			$res = $e;
 		}
