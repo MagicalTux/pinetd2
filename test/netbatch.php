929703cbda76cd75a49eef3b23810ba986e830a9
Mark Karpeles <mark@hell.ne.jp>
2009-09-07 07:35:19 +0000

    netbatch client api: improved api looks
    git-svn-id: https://ookoo.org/svn/pinetd2/trunk@386 bde33f66-d171-dc11-b289-00304882fe80

--- test/netbatch.php
+++ test/netbatch.php
@@ -1,5 +1,71 @@
 <?php
 
+class NetBatch_Process {
+	private $pid;
+	private $parent;
+	private $blocking;
+
+	public function __construct($parent, $pid) {
+		$this->parent = $parent;
+		$this->pid = $pid;
+		$this->blocking = true;
+	}
+
+	public function __destruct() {
+		if ($this->running())
+			$this->kill(9); // SIGKILL
+		$this->parent->freePid($this->pid);
+	}
+
+	public function setBlocking($blocking) {
+		$this->blocking = (bool)$blocking;
+	}
+
+	public function dump() {
+		return $this->parent->dump($this->pid);
+	}
+
+	public function eof($fd) {
+		return $this->parent->eof($fd, $this->pid);
+	}
+
+	public function read($fd, $size) {
+		return $this->parent->read($fd, $size, $this->pid, $this->blocking);
+	}
+
+	public function gets($fd, $size = NULL) {
+		return $this->parent->gets($fd, $size, $this->pid, $this->blocking);
+	}
+
+	public function wait() {
+		return $this->parent->wait($this->pid);
+	}
+
+	public function write($fd, $buf) {
+		return $this->parent->write($fd, $buf, $this->pid);
+	}
+
+	public function kill($signal = 15) {
+		return $this->parent->kill($signal, $this->pid);
+	}
+
+	public function close($fd) {
+		return $this->parent->close($fd, $this->pid);
+	}
+
+	public function getPid() {
+		return $this->pid;
+	}
+
+	public function running() {
+		return $this->parent->isRunning($this->pid);
+	}
+
+	public function exitCode() {
+		return $this->parent->getExitCode($this->pid);
+	}
+}
+
 class NetBatch {
 	private $fp;
 	private $name;
@@ -35,6 +101,12 @@ class NetBatch {
 		fclose($this->fp);
 	}
 
+	public function freePid($pid) {
+		unset($this->pipes[$pid]);
+		unset($this->pipes_buf[$pid]);
+		unset($this->returnCode[$pid]);
+	}
+
 	public function ident($login, $key) {
 		$this->sendPacket(sha1($key.$this->salt, true).$login, self::PKT_LOGIN);
 		$result = $this->readPacket();
@@ -59,13 +131,12 @@ class NetBatch {
 		if ($pid === '0') return false;
 
 		list(,$pid) = unpack('N', $pid);
-		var_dump($pid);
 		$this->running[$pid] = true;
 		$this->last_pid = $pid;
 		$this->pipes[$pid] = $pipes;
 		$this->pipes_buf[$pid] = array();
 
-		return $pid;
+		return new NetBatch_Process($this, $pid);
 	}
 
 	public function dump($pid = NULL) {
@@ -82,7 +153,7 @@ class NetBatch {
 		return !isset($this->pipes[$pid][$fd]);
 	}
 
-	public function read($fd, $size, $pid = NULL) {
+	public function read($fd, $size, $pid = NULL, $blocking) {
 		if (is_null($pid)) 
 			$pid = $this->last_pid;
 
@@ -109,8 +180,9 @@ class NetBatch {
 
 			if (!isset($this->pipes[$pid][$fd])) return false;
 
-			$this->getEvent();
+			if (!$this->getEvent($blocking)) break;
 		}
+		return false;
 	}
 
 	public function gets($fd, $size = NULL, $pid = NULL) {
@@ -153,8 +225,9 @@ class NetBatch {
 
 			if (!isset($this->pipes[$pid][$fd])) return false;
 
-			$this->getEvent();
+			if (!$this->getEvent($blocking)) break;
 		}
+		return false;
 	}
 
 	public function wait($pid = NULL) {
@@ -164,6 +237,18 @@ class NetBatch {
 			$this->getEvent();
 
 		return $this->returnCode[$pid];
+	}
+
+	public function getExitCode($pid) {
+		return $this->returnCode[$pid];
+	}
+
+	public function isRunning($pid) {
+		return isset($this->running[$pid]);
+	}
+
+	public function hasProcesses() {
+		return (bool)count($this->running);
 	}
 
 	public function write($fd, $data, $pid = NULL) {
@@ -176,8 +261,10 @@ class NetBatch {
 		$this->sendPacket(pack('NN', $pid, $signal), self::PKT_KILL);
 	}
 
-	protected function getEvent() {
-		$pkt = $this->readPacket();
+	protected function getEvent($blocking = true) {
+		$pkt = $this->readPacket($blocking);
+
+		if ($pkt === false) return false;
 
 		switch($this->type) {
 			case self::PKT_EOF:
@@ -201,6 +288,38 @@ class NetBatch {
 				var_dump($this->type);
 				break;
 		}
+		return true;
+	}
+
+	public function getActive(array $tmp) {
+		if (!$tmp)
+			throw new Exception('getActive() called without params');
+
+		$this->getEvent(false);
+
+		$list = array();
+		$keys = array();
+		foreach($tmp as $key => $process) {
+			$list[$process->getPid()] = $process;
+			$keys[$process->getPid()] = $key;
+		}
+
+		$final_list = array();
+		while(!$final_list) {
+			foreach($list as $pid => $process) {
+				if ($this->pipes_buf[$pid]) {
+					$final_list[$keys[$pid]] = $process;
+				}
+				
+				if (!$process->running())
+					$final_list[$keys[$pid]] = $process;
+			}
+
+			if (!$final_list)
+				$this->getEvent();
+		}
+
+		return $final_list;
 	}
 
 	public function close($fd, $pid = NULL) {
@@ -212,8 +331,14 @@ class NetBatch {
 		return fwrite($this->fp, pack('nn', $type, strlen($data)).$data);
 	}
 
-	protected function readPacket() {
+	protected function readPacket($blocking = true) {
 		if (feof($this->fp)) throw new Exception('Connection lost');
+
+		if (!$blocking) {
+			if (!stream_select($r = array($this->fp), $w = NULL, $e = NULL, 0))
+				return false;
+		}
+
 		$len = fread($this->fp, 4);
 		list(,$type,$len) = unpack('n2', $len);
 
@@ -228,15 +353,41 @@ class NetBatch {
 $netbatch = new NetBatch('127.0.0.1');
 $netbatch->ident('test','test');
 
+$ping = array();
+
+$ping[] = $netbatch->run(array('ping', '127.0.0.1', '-c', '5'));
+$ping[] = $netbatch->run(array('ping', 'google.fr', '-c', '5'));
+
+foreach($ping as $process)
+	$process->setBlocking(false);
+
+while($netbatch->hasProcesses()) {
+	foreach($netbatch->getActive($ping) as $key => $process) {
+		$line = $process->gets(1);
+		while($line !== false) {
+			echo 'PID#'.$process->getPid().': '.rtrim($line)."\n";
+			$line = $process->gets(1);
+		}
+
+		if (!$process->running()) {
+			echo 'PID#'.$process->getPid().' has exited with exit code '.$process->exitCode()."\n";
+			unset($ping[$key]);
+		}
+	}
+}
+
+
+/*
 echo "Running: php\n";
 
-$netbatch->run(array('php'), NULL, array('foo' => 'bougaga!'));
-$netbatch->write(0, '<?php print_r($_ENV);');
-$netbatch->close(0); // close stdin
+$process = $netbatch->run(array('php'), NULL, array('foo' => 'bougaga!'));
+$process->write(0, '<?php print_r($_ENV);');
+$process->close(0); // close stdin
 
-while(!$netbatch->eof(1))
-	var_dump(rtrim($netbatch->gets(1)));
+while(!$process->eof(1))
+	var_dump(rtrim($process->gets(1)));
 
-$netbatch->wait();
-$netbatch->dump();
+$process->wait();
+$process->dump();
+*/
 
