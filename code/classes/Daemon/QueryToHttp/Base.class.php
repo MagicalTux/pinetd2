7dd828640b4498fd74dd8e6a89a2abb702d85e95
Mark Karpeles <mark@hell.ne.jp>
2009-11-18 03:04:24 +0000

    new daemon: QueryToHttp
    git-svn-id: https://ookoo.org/svn/pinetd2/trunk@402 bde33f66-d171-dc11-b289-00304882fe80

---
+++ code/classes/Daemon/QueryToHttp/Base.class.php
@@ -0,0 +1,37 @@
+<?php
+/*   Portable INET daemon v2 in PHP
+ *   Copyright (C) 2007 Mark Karpeles <mark@kinoko.fr>
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program; if not, write to the Free Software
+ *   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+
+namespace Daemon\QueryToHttp;
+
+class Base extends \pinetd\TCP\Base {
+	public function __construct($port, $daemon, &$IPC, $node) {
+		// we can do stuff here if we need
+		parent::__construct($port, $daemon, $IPC, $node);
+	}
+
+	public function _ChildIPC_getUrl() {
+		return $this->getUrl();
+	}
+
+	public function getUrl() {
+		return $this->localConfig['url']['_'];
+	}
+}
+
---
+++ code/classes/Daemon/QueryToHttp/Client.class.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Daemon\QueryToHttp;
+
+class Client extends \pinetd\TCP\Client {
+	private $ch;
+
+	public function welcomeUser() {
+		return true;
+	}
+
+	public function sendBanner() {
+		$this->setMsgEnd('');
+	}
+
+	function shutdown() {
+	}
+
+	public function doResolve() {
+		return;
+	}
+
+	protected function parseLine($lin) {
+		$lin = rtrim($lin);
+		if (is_null($this->ch)) {
+			$this->ch = curl_init($this->IPC->getUrl());
+			curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($this->ch, CURLOPT_HEADER, true);
+			curl_setopt($this->ch, CURLOPT_HTTPHEADER, array('X-Remote-Ip' => $this->peer[0], 'X-Remote-Port' => $this->peer[1], 'X-Remote-Host' => $this->peer[2]));
+		}
+		curl_setopt($this->ch, CURLOPT_POSTFIELDS, http_build_query(array('input' => $lin)));
+
+		$res = curl_exec($this->ch);
+
+		$header_len = curl_getinfo($this->ch, CURLINFO_HEADER_SIZE);
+		$headers = substr($res, 0, $header_len);
+		$res = substr($res, $header_len);
+		$this->sendMsg($res);
+		$this->close(); // TODO: check if a header contains X-Socket: close and only close if present
+	}
+}
+
