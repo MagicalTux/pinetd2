1bf7b5b6392ca9612d919ebf053fbb4e434e6605
Mark Karpeles <mark@hell.ne.jp>
2010-01-22 12:25:28 +0000

    fixing is_callable() with method_exists()
    git-svn-id: https://ookoo.org/svn/pinetd2/trunk@436 bde33f66-d171-dc11-b289-00304882fe80

--- code/classes/pinetd/IPC.class.php
+++ code/classes/pinetd/IPC.class.php
@@ -262,7 +262,7 @@ class IPC {
 		} else {
 			$method = $call[2];
 			try {
-				if (is_callable(array($class, '_asyncPort_'.$method))) {
+				if (method_exists($class, '_asyncPort_'.$method)) {
 					$reply = array($call[0], $call[1]);
 					$res = call_user_func(array($class, '_asyncPort_'.$method), $call[3], $reply);
 					return;
