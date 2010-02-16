70dbea96984bf4ad2ee876521cdb5f7f12cf7933
Mark Karpeles <mark@hell.ne.jp>
2010-02-16 13:02:06 +0000

    Non-blocking sockets for a cleaner world
    git-svn-id: https://ookoo.org/svn/pinetd2/trunk@441 bde33f66-d171-dc11-b289-00304882fe80

--- code/classes/pinetd/IPC.class.php
+++ code/classes/pinetd/IPC.class.php
@@ -108,6 +108,7 @@ class IPC {
 	 */
 	public function registerSocketWait($fd, $callback, &$data) {
 		if (!is_array($data)) throw new \Exception('No data defined :(');
+		stream_set_blocking($fd, false);
 		$this->fds[(int)$fd] = array('fd'=>$fd, 'last_activity' => time(), 'callback' => $callback, 'data' => &$data);
 	}
 
