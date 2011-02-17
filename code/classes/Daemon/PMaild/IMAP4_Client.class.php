f7f23a2a8afc010056347295f04b064c28f50d75
Mark Karpeles <mark@hell.ne.jp>
2011-02-17 10:54:19 +0000

    switching reverseMap to forward uidmap
    git-svn-id: https://ookoo.org/svn/pinetd2/trunk@722 bde33f66-d171-dc11-b289-00304882fe80

--- code/classes/Daemon/PMaild/IMAP4_Client.class.php
+++ code/classes/Daemon/PMaild/IMAP4_Client.class.php
@@ -1014,14 +1014,14 @@ A OK FETCH completed
 				$t = 'UN'.strtoupper($param[$i]);
 			}
 			if (preg_match('/([0-9]+):([0-9]+)/', $t, $match)) {
-				if (!isset($this->reverseMap[(int)$match[1]])) return false;
-				if (!isset($this->reverseMap[(int)$match[2]])) return false;
-				$where[] = '(`mailid` BETWEEN '.($this->reverseMap[(int)$match[1]]).' AND '.($this->reverseMap[(int)$match[2]]).')';
+				if (!isset($this->uidmap[(int)$match[1]])) return false;
+				if (!isset($this->uidmap[(int)$match[2]])) return false;
+				$where[] = '(`mailid` BETWEEN '.($this->uidmap[(int)$match[1]]).' AND '.($this->uidmap[(int)$match[2]]).')';
 				continue;
 			}
 			if (preg_match('/([0-9]+):\\*/', $t, $match)) {
-				if (!isset($this->reverseMap[(int)$match[1]])) return false;
-				$where[] = '`mailid` >= '.($this->reverseMap[(int)$match[1]]);
+				if (!isset($this->uidmap[(int)$match[1]])) return false;
+				$where[] = '`mailid` >= '.($this->uidmap[(int)$match[1]]);
 				continue;
 			}
 			switch($t) {
